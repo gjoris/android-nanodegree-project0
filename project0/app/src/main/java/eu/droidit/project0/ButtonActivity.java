d9bd03cfc4845822b32c225c48f738f39bd0061a
Geroen Joris <info@droidit.eu>
2016-03-20 10:41:11 +0100

    Added Toast upon click, and cleaned up the labels.

--- project0/app/src/main/java/eu/droidit/project0/ButtonActivity.java
+++ project0/app/src/main/java/eu/droidit/project0/ButtonActivity.java
@@ -2,6 +2,12 @@ package eu.droidit.project0;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ButtonActivity extends AppCompatActivity {
 
@@ -9,5 +15,41 @@ public class ButtonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Map<Integer, Integer> buttonMap = new HashMap<>(6);
+
+        buttonMap.put(R.id.build_it_bigger_button, R.string.build_it_bigger_label);
+        buttonMap.put(R.id.library_button, R.string.library_label);
+        buttonMap.put(R.id.my_own_app_button, R.string.captone_label);
+        buttonMap.put(R.id.scores_button, R.string.scores_label);
+        buttonMap.put(R.id.spotify_streamer_button, R.string.spotify_streamer_label);
+        buttonMap.put(R.id.xyz_reader_button, R.string.xyz_reader_label);
+
+        for (final Map.Entry<Integer, Integer> buttonOnScreen : buttonMap.entrySet()) {
+            Button button = (Button) this.findViewById(buttonOnScreen.getKey());
+            if (button != null) {
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "This button will launch: " + getResources().getString(buttonOnScreen.getValue()), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+    }
+
+    private class ButtonRepresentation {
+        private int id;
+        private int stringRef;
+
+        public ButtonRepresentation(int id, int stringRef) {
+            this.id = id;
+            this.stringRef = stringRef;
+        }
     }
 }
